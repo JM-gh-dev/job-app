5ddfb959cbcf228f7270602bd51ee8fffac372bf
JM <jakubsocialbox@gmail.com>
2025-08-13 19:30:14 +0200

    post get item into database

--- project/app/main.py
+++ project/app/main.py
@@ -1,12 +1,29 @@
-from fastapi import FastAPI
-from . import models
-from .database import engine
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
+from . import models, schemas, crud
+from .database import SessionLocal, engine
 
 # Tworzymy tabelę, jeśli jej nie ma
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
 @app.get("/")
 def home():
     return {"message": "Działa!"}
+
+@app.post("/items/", response_model=schemas.Item)
+def create_item(item: schemas.ItemCreate, db: Session = Depends(get_db)):
+    return crud.create_item(db=db, item=item)
+
+@app.get("/items/", response_model=list[schemas.Item])
+def read_items(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
+    return crud.get_items(db=db, skip=skip, limit=limit)
+
