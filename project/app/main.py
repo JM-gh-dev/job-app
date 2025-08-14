7066466ea2d066894120bb40a0c61e25efde2445
JM <jakubsocialbox@gmail.com>
2025-08-14 12:30:05 +0200

    adding dashboards

--- project/app/main.py
+++ project/app/main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI, Depends
 from sqlalchemy.orm import Session
 from . import models, schemas, crud
 from .database import SessionLocal, engine
+from starlette.middleware.wsgi import WSGIMiddleware
+from .dashboard import dash_app
 
 # Tworzymy tabelę, jeśli jej nie ma
 models.Base.metadata.create_all(bind=engine)
@@ -27,3 +29,5 @@ def create_item(item: schemas.ItemCreate, db: Session = Depends(get_db)):
 def read_items(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
     return crud.get_items(db=db, skip=skip, limit=limit)
 
+# Podpinamy Dash pod FastAPI
+app.mount("/dash", WSGIMiddleware(dash_app.server))
