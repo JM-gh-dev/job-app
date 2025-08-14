f1ba938c493e55fe266d1435e588f0c15bd47e28
JM <jakubsocialbox@gmail.com>
2025-08-14 12:53:43 +0200

    update code to new features

--- project/app/main.py
+++ project/app/main.py
@@ -17,17 +17,17 @@ def get_db():
     finally:
         db.close()
 
-@app.get("/")
-def home():
-    return {"message": "Działa!"}
+#@app.get("/")
+#def home():
+#    return {"message": "Działa!"}
 
-@app.post("/items/", response_model=schemas.Item)
-def create_item(item: schemas.ItemCreate, db: Session = Depends(get_db)):
-    return crud.create_item(db=db, item=item)
+@app.post("/applications/", response_model=schemas.Application)
+def create_application(app_data: schemas.ApplicationCreate, db: Session = Depends(get_db)):
+    return crud.create_application(db, app_data)
 
-@app.get("/items/", response_model=list[schemas.Item])
-def read_items(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
-    return crud.get_items(db=db, skip=skip, limit=limit)
+@app.get("/applications/", response_model=list[schemas.Application])
+def read_applications(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    return crud.get_applications(db, skip=skip, limit=limit)
 
 # Podpinamy Dash pod FastAPI
 app.mount("/dash", WSGIMiddleware(dash_app.server))
