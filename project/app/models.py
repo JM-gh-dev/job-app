bbd6a67eaa29a3b654fa99a537fa9c80486ef812
JM <jakubsocialbox@gmail.com>
2025-08-14 13:17:40 +0200

    update code

--- project/app/models.py
+++ project/app/models.py
@@ -3,14 +3,15 @@ from .database import Base
 
 # Definicja tabeli w bazie
 class Application(Base):
-    __tablename__ = "items"
+    __tablename__ = "applications"
     id = Column(Integer, primary_key=True)
     firma = Column(String, index=True)
     stanowisko = Column(String)
     link = Column(String)
-    widełki_min = Column(Numeric)
-    widełki_max = Column(Numeric)
+    link2 = Column(String, nullable=True)
+    widełki_min = Column(Numeric, nullable=True)
+    widełki_max = Column(Numeric, nullable=True)
     rodzaj_umowy = Column(String)
     data_zlozenia = Column(Date)
     odpowiedz = Column(String)
-    description = Column(String)
+    description = Column(String, nullable=True)
